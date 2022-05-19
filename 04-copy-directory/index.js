bd707d8af7ce65674ca8fb3a4d16cd3fde4900b9
dmitrykaliasinski <d.k.akadiman@gmail.com>
2022-05-19 16:57:55 +0300

    feat: 4th task done

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,1 +1,23 @@
+const fs = require('fs');
+const path = require('path');
 
+fs.mkdir(path.join(__dirname, 'files-copy'), { recursive: true }, (error) => {
+  if (error) {
+    console.error(error.message);
+  }
+});
+
+fs.readdir(path.join(__dirname, 'files'), (error, files) => {
+  if (error) {
+    console.log(error);
+  } else {
+    files.forEach((file) =>
+      fs.copyFile(path.join(__dirname, 'files', file), path.join(__dirname, 'files-copy', file), (error) => {
+        if (error) {
+          console.log(error);
+        }
+      })
+    );
+    console.log('Copy files done');
+  }
+});
